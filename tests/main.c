97e8e6a142193ac1d5c17e2cdf0ed4a8a5b7c054
loris <loris.gaume@42.ch>
2023-10-29 03:50:53 +0100

    Start

---
+++ tests/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaume <lgaume@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/29 03:48:45 by lgaume            #+#    #+#             */
+/*   Updated: 2023/10/29 03:49:50 by lgaume           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/get_next_line.h"
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("tests/message.txt", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	return (0);
+}i
