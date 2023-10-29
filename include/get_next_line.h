3dec2a9706d2f2b10b8dd7b93811708a78e658d8
loris <loris.gaume@42.ch>
2023-10-29 03:56:02 +0100

    Header

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include "stdlib.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+typedef struct	s_list
+{
+	char 			*content;
+	struct s_list	*next;
+}	t_list;
+
+char		*get_next_line(int fd);
+
+#endif
